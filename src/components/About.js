d7ec44a2559207cddd881db92addc3b02012779e
Daniel <dkilcullen25@gmail.com>
2022-01-10 15:46:38 -0600

    commit tree 6a81d07fdcc636e018573f09f0c5ebfb52b82e21

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React from "react"
+import Links from './Links'
 
-function About() {
+// Update the About component so that:
+
+// If a prop of bio is passed to this component, it is displayed inside of a <p> tag
+// If a prop of bio is not passed down, or if bio is an empty string, don't display the <p> tag
+// Hint: you'll need to do some conditional rendering here to determine whether or not to display the <p> tag
+
+function About(props) {
+  console.log(props)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {/* //conditionally render p tag */}
+      { props.bio ? <p>{props.bio}</p> : ''}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
 
-export default About;
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { render } from "react-dom";
 
 function Home(props) {
+
   return (
     <div id="home">
       <h1 style={{ color: props.color }}>
         {props.username} is a Web Developer from {props.city}
       </h1>
     </div>
-  );
+  )
+  
 }
 
 export default Home;
